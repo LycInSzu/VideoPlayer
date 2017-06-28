277b0cc6574546ecbb942a3d580967358e74f526
liyuchong <liyuchong@gionee.com>
2017-06-28 19:17:38 +0800

    init MyVideoPlayer based on ijkplayer

---
+++ app/src/main/java/com/smoke/hisenberg/videotest/constant/MyConfig.java
@@ -0,0 +1,11 @@
+package com.smoke.hisenberg.videotest.constant;
+
+import android.os.Environment;
+
+/**
+ * Created by lyc on 17-6-28.
+ */
+
+public class MyConfig {
+    public static final String baseDirectory= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+}
